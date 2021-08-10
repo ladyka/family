7dca90052ecdcb27af8244826e3dc1cbc720c837
Eugene Kulik <yauhene.kulik@gmail.com>
2021-08-11 01:02:28 +0300

    family-12

--- src/test/groovy/by/ladyka/family/api/FamilyApiControllerTest.groovy
+++ src/test/groovy/by/ladyka/family/api/FamilyApiControllerTest.groovy
@@ -1,24 +1,49 @@
-//package by.ladyka.family.api
+package by.ladyka.family.api
+
+import by.ladyka.family.BaseWebTest
+import by.ladyka.family.data.PersonRelations
+import by.ladyka.family.data.Persons
+import by.ladyka.family.dto.PersonPage
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+
+class FamilyApiControllerTest extends BaseWebTest {
+    @Autowired
+    Persons persons
+
+    @Autowired
+    PersonRelations personRelations
+
+    def "GetById"() {
+        given:
+        def parents = personRelations.createCoupleHusbandAndWife()
+        def child = persons.create(name: "Child",)
+//        def fatherAndChild = personRelations.create(parent: parents.entity.parent,
+//                child: child.entity,
+//                relation: RelationType.PARENT_CHILD)
 //
-//import by.ladyka.family.BaseWebTest
-//import by.ladyka.family.data.Bundle
-//import by.ladyka.family.data.Persons
-//import by.ladyka.family.entity.Person
-//import org.springframework.beans.factory.annotation.Autowired
-//
-//class FamilyApiControllerTest extends BaseWebTest {
-//    @Autowired
-//    Persons persons
-//
-//    def "получение person по id"() {
-//        given:
-//        def pers = persons.create()
-//        def id = pers.getEntity().getId()
-//
-//        when:
-//        testRestUser.getForEntity("api/person/$id",[])
-//
-//        then:
-//
-//    }
-//}
+//        def matherAndChild = personRelations.create(parent: parents.entity.child,
+//                child: child.entity,
+//                relation: RelationType.PARENT_CHILD)
+
+        when:
+        def responseEntity = testRestUser.getForEntity("/api/person/$child.entity.id", PersonPage)
+
+        then:
+        responseEntity.getStatusCode() == HttpStatus.OK
+        def personDto = responseEntity.body.person
+        personDto.id == child.entity.id
+        personDto.name == child.entity.name
+        personDto.surname == child.entity.surname
+        personDto.fathername == child.entity.fathername
+        personDto.gender == child.entity.gender
+        personDto.email == child.entity.email
+        personDto.phone == child.entity.phone
+        personDto.wikilink == child.entity.wikilink
+
+
+        cleanup:
+        delete child
+        delete parents
+    }
+}
--- src/test/groovy/by/ladyka/family/data/PersonRelations.groovy
+++ src/test/groovy/by/ladyka/family/data/PersonRelations.groovy
@@ -2,9 +2,12 @@ package by.ladyka.family.data
 
 
 import by.ladyka.family.entity.PersonRelation
+import by.ladyka.family.entity.RelationType
 import by.ladyka.family.repositories.PersonRelationRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
 
+@Component
 class PersonRelations {
     @Autowired
     PersonRelationRepository personRelationRepository
@@ -18,7 +21,17 @@ class PersonRelations {
         bundle.entity = personRelationRepository.saveAndFlush(new PersonRelation(
                 parent: properties.parent ?: bundle.appendAndGet(persons.create(name: "defParent")),
                 child: properties.child ?: bundle.appendAndGet(persons.create(name: "defChaild")),
-                relation: properties.relation
+                relation: RelationType.PARENT_CHILD
+        ))
+        bundle
+    }
+
+    Bundle<PersonRelation, Long> createCoupleHusbandAndWife(Map properties = [:]) {
+        def bundle = new Bundle<PersonRelation, Long>(personRelationRepository)
+        bundle.entity = personRelationRepository.saveAndFlush(new PersonRelation(
+                parent: properties.parent ?: bundle.appendAndGet(persons.create(name: "Husband", gender: true)),
+                child: properties.child ?: bundle.appendAndGet(persons.create(name: "Wife", gender: false)),
+                relation: RelationType.MARRIAGE
         ))
         bundle
     }
